5b15611a0c5231e46bbca359acd30a885e0472f2
Dharmik-dbcorp <dharmikbshah@gmail.com>
2020-10-30 12:48:49 +0530

    Added fetch all patients API

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Dharmik-dbcorp/SampleRestApiGO/helper"
 	"github.com/Dharmik-dbcorp/SampleRestApiGO/models"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 //Connection mongoDB with helper class
@@ -32,10 +33,47 @@ func createPatient(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
+func getPatients(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	
+	// created Patients array
+	var patients []models.Patient
+
+	cur, err := collection.Find(context.TODO(), bson.M{})
+
+	if err != nil {
+		helper.GetError(err, w)
+		return
+	}
+
+	// Close the cursor once finished
+	defer cur.Close(context.TODO())
+
+	for cur.Next(context.TODO()) {
+
+		var patient models.Patient
+		// & character returns the memory address of the following variable.
+		err := cur.Decode(&patient)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// add item our array
+		patients = append(patients, patient)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(patients) // encode similar to serialize process.
+}
+
 func main() {
 	//Init Router
 	r := mux.NewRouter()
 
+	r.HandleFunc("/api/patients", getPatients).Methods("GET")
 	r.HandleFunc("/api/patient", createPatient).Methods("POST")
 
 	config := helper.GetConfiguration()
